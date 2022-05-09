40076a9254c075b6344b87fd3dd23f7b0456a961
Monika Swoboda <mon.swoboda@gmail.com>
2022-05-09 13:34:10 +0200

    init project

---
+++ src/js/script.js
@@ -0,0 +1,40 @@
+/* global Handlebars, utils, dataSource */ // eslint-disable-line no-unused-vars
+
+{
+'use strict';
+
+  const select = {
+    templateOf: {
+      book: '#template-book', 
+    },
+    containerOf: {
+      bookList: '.books-list',
+      booksPanel: '.books-panel',
+    },
+  };
+
+  const templates = {
+    bookCard: Handlebars.compile(document.querySelector(select.templateOf.book).innerHTML),
+  }; 
+
+const renderBookList = function(){
+    for(const book of dataSource.books){
+  
+      const generatedHTML = templates.bookCard(book);
+      console.log('generatedHTML:', generatedHTML);
+
+      const generatedDOM = utils.createDOMFromHTML(generatedHTML);
+      console.log('generatedDOM:', generatedDOM);
+
+      const bookContainer = document.querySelector(select.containerOf.bookList);
+      console.log('bookContainer:', bookContainer);
+
+      bookContainer.appendChild(generatedDOM);
+    }
+  }; 
+
+    renderBookList();
+
+}  
+ 
+
